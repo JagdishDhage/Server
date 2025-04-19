f1eb0b0049d449890a711823372085cd3cb2734e
Jagdish Dhage <140432508+JagdishDhage@users.noreply.github.com>
2025-04-20 00:19:44 +0530

    Update User.js

--- Controller/User.js
+++ Controller/User.js
@@ -1,5 +1,5 @@
 const User = require('../Model/User');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const multer=require('multer')
 const sendEmail=require('../Middlewares/SendEmail')
